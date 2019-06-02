3e2b51a55488f8efc37c0c50770b5e5f1a759d2f
Indexyz <jiduye@gmail.com>
2019-06-02 12:32:00 +0800

    feat: update codes

---
+++ ping/utils.go
@@ -0,0 +1,32 @@
+package ping
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
+
+func timeIt(f func() interface{}) (int64, interface{}) {
+	startAt := time.Now()
+	res := f()
+	endAt := time.Now()
+	return endAt.UnixNano() - startAt.UnixNano(), res
+}
+
+// FormatIP - trim spaces and format IP
+//
+// IP - the provided IP
+//
+// string - return "" if the input is neither valid IPv4 nor valid IPv6
+//          return IPv4 in format like "192.168.9.1"
+//          return IPv6 in format like "[2002:ac1f:91c5:1::bd59]"
+func FormatIP(IP string) string {
+
+	host := strings.Trim(IP, "[ ]")
+	if parseIP := net.ParseIP(host); parseIP != nil && parseIP.To4() == nil {
+		host = fmt.Sprintf("[%s]", host)
+	}
+
+	return host
+}
